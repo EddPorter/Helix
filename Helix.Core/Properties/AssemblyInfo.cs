9f5064099cb614512ee38c8d0ae5f71a665aee89
Edd Porter <git@eddporter.com>
2015-07-22 22:28:57 +0100

    Create core library project.

---
+++ Helix.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿//  Helix - A Genetic Programming Library
+//  Copyright (C) 2015 Edd Porter<helix@eddporter.com>
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helix.Core")]
+[assembly: AssemblyDescription("Core libraries for the Helix Genetic Programming Library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Helix")]
+[assembly: AssemblyCopyright("Copyright © 2015 Edd Porter")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1.0")]
